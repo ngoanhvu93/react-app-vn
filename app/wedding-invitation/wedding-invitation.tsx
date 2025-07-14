9d1ed86049dba3976d7626f1c63d7fc9a62bff9c
Vu Ngo <ngoanhvu110293@gmail.com>
2025-07-14 21:28:04 +0700

    Refactor WeddingInvitation component by removing outdated loading animation and styles; streamline loading state handling for improved user experience.

--- app/wedding-invitation/wedding-invitation.tsx
+++ app/wedding-invitation/wedding-invitation.tsx
@@ -62,6 +62,18 @@ export default function WeddingInvitation() {
     }
   };
 
+  if (isLoading) {
+    return (
+      <div className="bg-gradient-to-br absolute top-1/2 -translate-y-1/2  left-1/2 -translate-x-1/2 w-full h-screen from-red-50 via-pink-50 to-rose-100 overflow-hidden flex items-center justify-center pb-28">
+        <DotLottieReact
+          src="https://lottie.host/a9035f38-a11a-4ea6-a5b2-6ccab0bf3d71/jLGLmOtYkz.lottie"
+          loop
+          autoplay
+        />
+      </div>
+    );
+  }
+
   const AccountNumberDisplay = ({
     accountNumber,
     accountType,
@@ -97,18 +109,6 @@ export default function WeddingInvitation() {
       </div>
     );
   };
-
-  if (isLoading) {
-    return (
-      <div className="bg-gradient-to-br absolute top-1/2 -translate-y-1/2  left-1/2 -translate-x-1/2 w-full h-screen from-red-50 via-pink-50 to-rose-100 overflow-hidden flex items-center justify-center pb-28">
-        <DotLottieReact
-          src="https://lottie.host/a9035f38-a11a-4ea6-a5b2-6ccab0bf3d71/jLGLmOtYkz.lottie"
-          loop
-          autoplay
-        />
-      </div>
-    );
-  }
 
   return (
     <div className="relative bg-gradient-to-br from-red-50 via-pink-50 to-rose-100 overflow-hidden max-w-md mx-auto pb-8">
